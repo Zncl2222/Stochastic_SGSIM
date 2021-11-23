4a86b149a4a07caf9747a07efc5d30e9c13f97dc
Zncl2222 <3002shinning@gmail.com>
2021-11-23 17:51:09 +0800

    Fix lay out

--- UC_SGSIM.c
+++ UC_SGSIM.c
@@ -84,25 +84,32 @@ void LUdecomposition(double** a ,double* b, double* x , int n)
    double l[10][10],u[10][10];
    double c[10];
  
-   for (i = 0; i < n; i++) {
-      for (j = 0; j < n; j++) {
-         if (j < i)
-         l[j][i] = 0;
-         else {
-            l[j][i] = a[j][i];
-            for (k = 0; k < i; k++) {
-               l[j][i] = l[j][i] - l[j][k] * u[k][i];
+   for (i = 0; i < n; i++) 
+   {
+        for (j = 0; j < n; j++) 
+        {
+            if (j < i)
+            l[j][i] = 0;
+            else 
+            {
+                l[j][i] = a[j][i];
+                for (k = 0; k < i; k++) 
+                {
+                    l[j][i] = l[j][i] - l[j][k] * u[k][i];
+                }
             }
-         }
       }
-      for (j = 0; j < n; j++) {
-         if (j < i)
-         u[i][j] = 0;
-         else if (j == i)
-         u[i][j] = 1;
-         else {
+      for (j = 0; j < n; j++) 
+      {
+        if (j < i)
+        u[i][j] = 0;
+        else if (j == i)
+        u[i][j] = 1;
+        else
+        {
             u[i][j] = a[i][j] / l[i][i];
-            for (k = 0; k < i; k++) {
+            for (k = 0; k < i; k++) 
+            {
                u[i][j] = u[i][j] - ((l[i][k] * u[k][j]) / l[i][i]);
             }
          }
