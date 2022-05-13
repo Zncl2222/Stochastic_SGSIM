e6f1f4abfdeb775c84b805d8719618188cb86df5
Zncl2222 <3002shinning@gmail.com>
2022-05-13 21:48:23 +0800

    code refactoring

---
+++ UC_SGSIM_py/__init__.py
@@ -0,0 +1,8 @@
+from UC_SGSIM_py import Krige
+from UC_SGSIM_py.Krige import Kriging, SimpleKrige
+from UC_SGSIM_py.Cov_Model import Gaussian
+from UC_SGSIM_py.Plot import Visualize
+from UC_SGSIM_py.UC_SGSIM import Simulation
+
+
+__all__ = ["TEST123"]
