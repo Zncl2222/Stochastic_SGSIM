62f2cab214155c7f31f5b1b268bb89ebf9326522
Zncl2222 <3002shinning@gmail.com>
2022-05-14 19:00:06 +0800

    Adjust variogram plot

--- UC_SGSIM_py/Plot/Plot.py
+++ UC_SGSIM_py/Plot/Plot.py
@@ -20,21 +20,21 @@ class Visualize(Plot_Base):
                 plt.figure(77879,figsize=self.figsize)
                 plt.plot(self.RandomField[:,i]*std+mean)
                 plt.title("Realizations: "+self.model_name,fontsize=20)
-                plt.xlabel("Distance(m)",fontsize=20)
+                plt.xlabel("Distance(-)",fontsize=20)
                 plt.axhline(y=mean, color='r', linestyle='--',zorder=1)
-                plt.ylabel("Thermal Conductivity",fontsize=20)
+                plt.ylabel("Y",fontsize=20)
             
         else:
             for item in n:
                 plt.figure(77879,figsize=self.figsize)
                 plt.plot(self.RandomField[:,item]*std+mean)
                 plt.title("Realizations: "+self.model_name,fontsize=20)
-                plt.xlabel("Distance(m)",fontsize=20)
+                plt.xlabel("Distance(-)",fontsize=20)
                 plt.axhline(y=mean, color='r', linestyle='--',zorder=1)
-                plt.ylabel("Thermal Conductivity",fontsize=20)
+                plt.ylabel("Y",fontsize=20)
 
         
-    def Statistic_Plot(self,mean=0,std=1):
+    def Variance_Plot(self,mean=0,std=1):
 
         Zmean=np.zeros(len(self.RandomField[:,0]))
 
@@ -44,7 +44,7 @@ class Visualize(Plot_Base):
         
         plt.figure(5212,figsize=self.figsize)
         plt.plot(Zmean,'-s',color='k',markeredgecolor='k',markerfacecolor='y')
-        plt.xlabel("Distance(m)",fontsize=20)
+        plt.xlabel("Distance(-)",fontsize=20)
         plt.ylabel("Mean",fontsize=20)
         plt.axhline(y=mean, color='r', linestyle='--',zorder=1)
         plt.xticks(fontsize=17),plt.yticks(fontsize=17)
@@ -58,44 +58,85 @@ class Visualize(Plot_Base):
         plt.figure(52712,figsize=self.figsize)
         plt.plot(Zvar,'-o',color='k',markeredgecolor='k',markerfacecolor='r')
         #plt.title("Variance",fontsize=24)
-        plt.xlabel("Distance(m)",fontsize=20)
+        plt.xlabel("Distance(-)",fontsize=20)
         plt.ylabel("Variance",fontsize=20)
         plt.axhline(y=std**2, color='b', linestyle='--',zorder=1)
         plt.xticks(fontsize=17),plt.yticks(fontsize=17)
-    
-    def Variogram_Plot(self):
+  
+    def CDF_Plot(self):
         
+        X = self.RandomField[:,1]
+
+        mu = np.mean(X)
+        sigma = np.std(X)
+        n_bins = 50
+
+        fig, ax = plt.subplots(figsize=(8, 4))
+
+        n, bins, patches = ax.hist(X, n_bins, density=True, histtype='step',
+                                   cumulative=True, label='Empirical')
+
+        y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+             np.exp(-0.5 * (1 / sigma * (bins - mu))**2))
+        y = y.cumsum()
+        y /= y[-1]
+        
+        ax.plot(bins, y, 'k--', linewidth=1.5, label='Theoretical')
+
+        ax.grid(True)
+        ax.legend(loc='right')
+        ax.set_title('Cumulative step histograms')
+        ax.set_xlabel('Random Variable (mm)')
+        ax.set_ylabel('Occurrence')
+        
+    def HIST(self):
+       
+        X=self.RandomField[:,1]
+
+        mu = np.mean(X) 
+        sigma = np.std(X)
+
+        num_bins = 50
+        plt.figure(num=1151)
+        n, bins, patches = plt.hist(X, num_bins,  
+                                    density = 1,  
+                                    color ='blue', 
+                                    alpha = 0.5,
+                                    edgecolor='k') 
+
+        y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *np.exp(-0.5 * (1 / sigma * (bins - mu))**2)) 
+        
+        plt.plot(bins, y, '--', color ='black')
+
+        plt.xlabel('X-Axis') 
+        plt.ylabel('Y-Axis') 
+
+        plt.title('Histogram') 
+    
+    def Variogram_Plot(self, Variogram):
+
         start_time=time.time()
         
-        model_len=self.size
-        
-        Vario=np.zeros([len(self.hs),self.nR])
-        
-        x=np.linspace(0,self.size-1,model_len).reshape(model_len,1)
-        
         for i in range(self.nR):
-            L=np.hstack([x,self.RandomField[:,i].reshape(model_len,1)])
-            
-            Vario[:,i]=self.model.Variogram(L)
             plt.figure(123456,figsize=(10,6))
-            plt.plot(Vario[:,i],alpha=0.1)
+            plt.plot(Variogram[:,i],alpha=0.1)
             plt.title("Model: "+self.model_name,fontsize=20)
             plt.xlabel("Lag(m)",fontsize=20)
             plt.ylabel("Variogram",fontsize=20)
             plt.xticks(fontsize=17),plt.yticks(fontsize=17)
-            print('Progress = %.2f' % (i/self.nR*100)+'%', end='\r')
+            #print('Progress = %.2f' % (i/self.nR*100)+'%', end='\r')
             
         plt.plot(self.model.Var_compute(self.hs),'o',markeredgecolor='k',markerfacecolor='w')
         
         Vario_mean=np.zeros(len(self.hs))
         for i in range(len(self.hs)):
             
-            Vario_mean[i]=np.mean(Vario[i,:])
+            Vario_mean[i]=np.mean(Variogram[i,:])
             
         plt.plot(Vario_mean,'--',color='blue')
         
-        print('Progress = %.2f' % 100+'%\n', end='\r')
+        #print('Progress = %.2f' % 100+'%\n', end='\r')
         
         end_time=time.time()
         
-        print('Time = ', end_time-start_time,'s')
+        #print('Time = ', end_time-start_time,'s')
