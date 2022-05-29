41f39f16893127cbfed021010bb829188c012cd2
Zncl2222 <3002shinning@gmail.com>
2022-05-29 15:19:42 +0800

    feat: import new class Simulation_byC
    import new class Simulation_byC for dll calculation.

--- UC_SGSIM_py/__init__.py
+++ UC_SGSIM_py/__init__.py
@@ -1,8 +1,9 @@
 from UC_SGSIM_py import Krige
 from UC_SGSIM_py.Krige import Kriging, SimpleKrige
-from UC_SGSIM_py.Cov_Model import Gaussian
+from UC_SGSIM_py.Cov_Model import Gaussian, Spherical
 from UC_SGSIM_py.Plot import Visualize
-from UC_SGSIM_py.UC_SGSIM import Simulation
+from UC_SGSIM_py.UC_SGSIM import Simulation, Simulation_byC
 
 
-__all__ = ["TEST123"]
+
+__all__ = [""]
