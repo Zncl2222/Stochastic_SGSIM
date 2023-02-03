ba4bfd7e745004b8cef1d11a34fabf239875aa9c
Zncl2222 <3002shinning@gmail.com>
2023-02-03 01:53:46 +0000

    build(c_core): add executable option in no-cmake compiling and update .so lib

---
+++ uc_sgsim/c_core/build.sh
@@ -0,0 +1,19 @@
+is_excute=false
+file_name="uc_sgsim"
+
+if $is_excute; then
+    # compile executable
+    echo "Building executable file"
+    file=$file_name".out"
+    gcc -o uc_sgsim.out \
+    ../../UC_SGSIM_c_example.c \
+    src/sgsim.c  src/krige.c  src/cov_model.c \
+    src/variogram.c  src/sort_tools.c  src/random_tools.c  src/matrix_tools.c -lm
+else
+    # compile shared lib
+    echo "Building shared library"
+    file=$file_name".so"
+    gcc -shared -fPIC -o $file  \
+    src/sgsim.c  src/krige.c  src/cov_model.c \
+    src/variogram.c  src/sort_tools.c  src/random_tools.c  src/matrix_tools.c
+fi
--- uc_sgsim/c_core/build_so_lib.sh
+++
@@ -1,1 +0,0 @@
-gcc -shared -fPIC -o uc_sgsim.so  model/sgsim.c  model/krige.c  model/cov_model.c  model/variogram.c  model/sort_tools.c  model/random_tools.c  model/matrix_tools.c
