3ae72fb772c52252dd07822293f134570977ddcc
Zncl2222 <3002shinning@gmail.com>
2023-06-09 07:32:02 +0000

    build(cmake): adjust cmake options for .exe or .so compiling
    1. add -s options for cmake_build.sh to compile the exe or so. Compile exe as default
    
    2. add -O3 flags to optimze the program

--- cmake_build.sh
+++ cmake_build.sh
@@ -1,6 +1,30 @@
+#!/bin/bash
+
+is_execute=true
+
+# Parse command-line arguments
+while getopts ":s" opt; do
+    case $opt in
+        s)
+            is_execute=false
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
 mkdir cmake_build
 cd cmake_build
-cmake ..
+
+if [ "$is_execute" = true ]; then
+    echo "Compile the code into an executable file !"
+    cmake -DIS_EXECUTE=ON ..
+else
+    echo "Compile the code into a shared library (dynamic library) !"
+    cmake -DIS_EXECUTE=OFF ..
+fi
+
 make
 make test
 make memcheck
