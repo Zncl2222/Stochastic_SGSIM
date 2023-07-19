0aa16b23311d2a66a51aa2633e5a3d167f410565
Zncl2222 <3002shinning@gmail.com>
2023-07-19 09:41:57 +0000

    feat(plot): add more custom plotting options

--- uc_sgsim/plot/plot.py
+++ uc_sgsim/plot/plot.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import matplotlib.pyplot as plt
 import numpy as np
+
 from uc_sgsim.exception import VariogramDoesNotCompute
 from uc_sgsim.plot.base import PlotBase
 from ..cov_model.base import CovModel
@@ -46,29 +49,61 @@ class SgsimPlot(PlotBase):
     def vsill(self) -> float:
         return self.__vsill
 
-    def plot(self, realizations: list[int] = None, mean=0) -> None:
+    def plot(
+        self,
+        realizations: list[int] = None,
+        mean: float | int = 0,
+        fontsize: int = 20,
+        y_title: str = 'Y',
+        axhline_color: str = 'r',
+        axhline_style: str = '--',
+        axhline_zorder: int = 1,
+    ) -> None:
         realization_number = len(self.random_field[:, 0])
         if realizations is None:
             for i in range(realization_number):
                 plt.figure(self.curr_fig_num, figsize=self.figsize)
                 plt.plot(self.random_field[i, :] + mean)
-                plt.title('Realizations: ' + self.vmodel_name, fontsize=20)
-                plt.xlabel(self.xlabel, fontsize=20)
-                plt.axhline(y=mean, color='r', linestyle='--', zorder=1)
-                plt.ylabel('Y', fontsize=20)
+                plt.title('Realizations: ' + self.vmodel_name, fontsize=fontsize)
+                plt.xlabel(self.xlabel, fontsize=fontsize)
+                plt.axhline(
+                    y=mean,
+                    color=axhline_color,
+                    linestyle=axhline_style,
+                    zorder=axhline_zorder,
+                )
+                plt.ylabel(y_title, fontsize=fontsize)
         elif type(realizations) == list:
             for item in realizations:
                 plt.figure(self.curr_fig_num, figsize=self.figsize)
                 plt.plot(self.random_field[:, item] + mean)
-                plt.title('Realizations: ' + self.vmodel_name, fontsize=20)
-                plt.xlabel(self.xlabel, fontsize=20)
-                plt.axhline(y=mean, color='r', linestyle='--', zorder=1)
-                plt.ylabel('Y', fontsize=20)
+                plt.title('Realizations: ' + self.vmodel_name, fontsize=fontsize)
+                plt.xlabel(self.xlabel, fontsize=fontsize)
+                plt.axhline(
+                    y=mean,
+                    color=axhline_color,
+                    linestyle=axhline_style,
+                    zorder=axhline_zorder,
+                )
+                plt.ylabel(y_title, fontsize=fontsize)
         else:
             raise TypeError("Argument 'realizations' should be list or None")
         self.curr_fig_num += 1
 
-    def mean_plot(self, mean=0) -> None:
+    def mean_plot(
+        self,
+        mean: int | float = 0,
+        fmt: str = '-s',
+        linecolor: str = 'k',
+        markeredgecolor: str = 'k',
+        markerfacecolor: str = 'y',
+        fontsize: int = 20,
+        axhline_color: str = 'r',
+        axhline_style: str = '--',
+        axhline_zorder: int = 1,
+        xticks_fontsize: int = 17,
+        yticks_fontsize: int = 17,
+    ) -> None:
         zmean = np.zeros(len(self.random_field[0, :]))
         for i in range(len(self.random_field[0, :])):
             zmean[i] = np.mean(self.random_field[:, i] + mean)
@@ -76,15 +111,21 @@ class SgsimPlot(PlotBase):
         plt.figure(self.curr_fig_num, figsize=self.figsize)
         plt.plot(
             zmean,
-            '-s',
-            color='k',
-            markeredgecolor='k',
-            markerfacecolor='y',
+            fmt,
+            color=linecolor,
+            markeredgecolor=markeredgecolor,
+            markerfacecolor=markerfacecolor,
         )
-        plt.xlabel(self.xlabel, fontsize=20)
-        plt.ylabel('Mean', fontsize=20)
-        plt.axhline(y=mean, color='r', linestyle='--', zorder=1)
-        plt.xticks(fontsize=17), plt.yticks(fontsize=17)
+        plt.xlabel(self.xlabel, fontsize=fontsize)
+        plt.ylabel('Mean', fontsize=fontsize)
+        plt.axhline(
+            y=mean,
+            color=axhline_color,
+            linestyle=axhline_style,
+            zorder=axhline_zorder,
+        )
+        plt.xticks(fontsize=xticks_fontsize)
+        plt.yticks(fontsize=yticks_fontsize)
         self.curr_fig_num += 1
 
     def variance_plot(self) -> None:
@@ -163,15 +204,24 @@ class SgsimPlot(PlotBase):
         plt.title('Histogram, x = ' + str(x_location))
         self.curr_fig_num += 1
 
-    def variogram_plot(self) -> None:
+    def variogram_plot(
+        self,
+        fontsize: int = 20,
+        alpha: int | float = 0.1,
+        xticks_fontsize: int = 17,
+        yticks_fontsize: int = 17,
+        mean_color: str = 'blue',
+        mean_linestyle: str = '--',
+    ) -> None:
         self.__variogram_validate()
         for i in range(self.realization_number):
             plt.figure(self.curr_fig_num, figsize=(10, 6))
-            plt.plot(self.variogram[i, :], alpha=0.1)
-            plt.title('Model: ' + self.vmodel_name, fontsize=20)
-            plt.xlabel('Lag(m)', fontsize=20)
-            plt.ylabel('Variogram', fontsize=20)
-            plt.xticks(fontsize=17), plt.yticks(fontsize=17)
+            plt.plot(self.variogram[i, :], alpha=alpha)
+            plt.title('Model: ' + self.vmodel_name, fontsize=fontsize)
+            plt.xlabel('Lag(m)', fontsize=fontsize)
+            plt.ylabel('Variogram', fontsize=fontsize)
+            plt.xticks(fontsize=xticks_fontsize)
+            plt.yticks(fontsize=yticks_fontsize)
 
         self.theory_variogram_plot()
 
@@ -179,21 +229,28 @@ class SgsimPlot(PlotBase):
         for i in range(len(self.vbandwidth)):
             Vario_mean[i] = np.mean(self.variogram[:, i])
 
-        plt.plot(Vario_mean, '--', color='blue')
+        plt.plot(Vario_mean, mean_linestyle, color=mean_color)
         self.curr_fig_num += 1
 
-    def theory_variogram_plot(self, fig: int = None) -> None:
+    def theory_variogram_plot(
+        self,
+        fig: int = None,
+        fontsize: int = 20,
+        fmt: str = 'o',
+        markeredgecolor: str = 'k',
+        markerfacecolor: str = 'w',
+    ) -> None:
         if fig is not None:
             plt.figure(fig, figsize=self.figsize)
         plt.plot(
             self.vmodel.var_compute(self.vbandwidth),
-            'o',
-            markeredgecolor='k',
-            markerfacecolor='w',
+            fmt,
+            markeredgecolor=markeredgecolor,
+            markerfacecolor=markerfacecolor,
         )
-        plt.title('Model: ' + self.vmodel_name, fontsize=20)
-        plt.xlabel('Lag(m)', fontsize=20)
-        plt.ylabel('Variogram', fontsize=20)
+        plt.title('Model: ' + self.vmodel_name, fontsize=fontsize)
+        plt.xlabel('Lag(m)', fontsize=fontsize)
+        plt.ylabel('Variogram', fontsize=fontsize)
 
     def __variogram_validate(self) -> None:
         if type(self.variogram) == int:
