a93d374ca86a9074dc38e95d13320720f0aa505b
Zncl2222 <3002shinning@gmail.com>
2023-09-16 10:26:23 +0800

    fix(sort_tools): fix the function definition in header file

--- uc_sgsim/c_core/include/sort_tools.h
+++ uc_sgsim/c_core/include/sort_tools.h
@@ -2,10 +2,12 @@
 #ifndef UC_SGSIM_C_CORE_INCLUDE_SORT_TOOLS_H_
 #define UC_SGSIM_C_CORE_INCLUDE_SORT_TOOLS_H_
 
-void swap(double** x, int i, int j);
+void swaprows(double** x, int i, int j);
 
 void quicksort2d(double** array, int front, int end);
 
-int Partition2d(double** array, int front, int end);
+int partition2d(double** array, int front, int end);
+
+void quickselect2d(double** array, int front, int end, int k);
 
 #endif  // UC_SGSIM_C_CORE_INCLUDE_SORT_TOOLS_H_
